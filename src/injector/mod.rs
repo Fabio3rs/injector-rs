d5ae348c6acaca927784c6163960d66cb24ffae1
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2020-04-25 15:38:30 -0300

    injector lib update

--- src/injector/mod.rs
+++ src/injector/mod.rs
@@ -4,8 +4,8 @@
 
 pub mod injector
 {
+    #![allow(dead_code)]
     extern crate winapi;
-    use std::io::Error;
 
     struct Vpdata
     {
@@ -54,23 +54,25 @@ pub mod injector
         }
     }
 
+    #[allow(dead_code)]
     #[repr(packed(1))]
     pub struct RegPack {
         // PUSHFD / POPFD
-        ef: u32,
+        pub ef: u32,
         // PUSHAD/POPAD -- must be the lastest fields (because of esp)
-        edi: u32,
-        esi: u32,
-        ebp: u32,
-        esp: u32,
-        ebx: u32,
-        edx: u32,
-        ecx: u32,
-        eax: u32,
+        pub edi: u32,
+        pub esi: u32,
+        pub ebp: u32,
+        pub esp: u32,
+        pub ebx: u32,
+        pub edx: u32,
+        pub ecx: u32,
+        pub eax: u32,
         // RET ADDRESS
-        retn: u32,
+        pub retn: u32,
     }
 
+    #[allow(dead_code)]
     #[repr(packed(1))]
     pub struct InlineCode
     {
@@ -83,6 +85,9 @@ pub mod injector
 
         push: u8,
         pushaddr: u32,
+
+        push2: u8,
+        oldcalladdr: u32,
 
         call: u8,
         addr: u32,
@@ -114,12 +119,13 @@ pub mod injector
 				    pushesp: 0x54,			   // push esp
 
 				    push: 0x68, pushaddr: 0,      // push address
+                    push2: 0x68, oldcalladdr: 0x00,         // push <retnaddr>
 				    //0x68, uintptr_t(c), // push c
 				    //0x68, 0x00,         // push <retnaddr>
 
 				    call: 0xE8, addr: 0x00,			// call callwrapper
 
-				    addesp: 0xC483, addespnum: 8,		// add esp, 8
+				    addesp: 0xC483, addespnum: 12,		// add esp, 12
 
 				    sub: 0x80, data1: 0x0410246C, // sub[esp + 12 + 4], 4
 
@@ -140,15 +146,15 @@ pub mod injector
         var as usize
     }
 
-    extern "C" fn make_inline_wrapper(funptr : u32, reg_pack : *mut RegPack)
+    extern "C" fn make_inline_wrapper(oldcalladdr : u32, funptr : u32, reg_pack : *mut RegPack)
     {
-        let foo = add_as_fun_ptr::<extern "C" fn(*mut RegPack)>(&funptr);
-        foo(reg_pack);
+        let foo = add_as_fun_ptr::<extern "C" fn(*mut RegPack, u32)>(&funptr);
+        foo(reg_pack, oldcalladdr);
     }
 
     pub fn make_inline<T>(addr : u32, target : *mut T)
     {
-        if let Some(x) = unsafe { &mut INLINE_LIST }
+        if let Some(_x) = unsafe { &mut INLINE_LIST }
         {
             
 		}else
@@ -165,62 +171,14 @@ pub mod injector
                     if let Some(el) = x.back_mut()
                     {
                         make_call(address_of(&el.call) as *mut u8, make_inline_wrapper as *mut u8, true) as u32;
-                        el.pushaddr = (target as u32);
+                        el.pushaddr = target as u32;
 
-                        make_call(addr as *mut u8, address_of(el) as *mut u8, true) as u32;
+                        el.oldcalladdr = make_call(addr as *mut u8, address_of(el) as *mut u8, true) as u32;
 			        }     
 			    }
             },
-            None => { print_message("INLINE_LIST is None"); },
+            None => { let _ = print_message("INLINE_LIST is None"); },
 	    }
-    }
-
-    /*
-    struct newCode
-		    {
-			    uint8_t pushad; // pushad
-
-			    // add[esp + 12], 4 
-			    uint8_t add;
-			    uint32_t data;
-
-			    uint8_t pushfd; // pushfd
-
-			    uint8_t pushesp; // push esp
-
-			    uint8_t push;
-			    uintptr_t pushaddr;
-			    uint8_t push2;
-			    uintptr_t useraddr;
-			    uint8_t push3;
-			    uintptr_t retaddr;
-
-			    // call callwrapper
-			    uint8_t call;
-			    uint32_t addr;
-
-			    // add esp, 16
-			    uint16_t addesp;
-			    uint8_t addespnum;
-
-			    // sub[esp + 12 + 4], 4
-			    uint8_t sub;
-			    uint32_t data1;
-
-
-			    uint8_t popfd; // popfd
-			    uint8_t popad; // popad
-
-
-			    // retn x
-			    uint8_t retn;
-			    uint16_t retnsiz;
-		    } PACKED;
-    */
-
-    pub fn it_works()
-    {
-        assert_eq!(2 + 2, 4);
     }
 
     pub fn read_memory<T>(mem : *mut T, vp : bool) -> T
@@ -234,6 +192,11 @@ pub mod injector
         let _scopevp = Vpdata::new(mem, std::mem::size_of::<T>(), vp);
         unsafe { std::ptr::write_unaligned(mem, val); }
     }
+
+    pub fn write_memory_iptr<T>(mem : u32, val : T, vp : bool)
+    {
+        write_memory(mem as *mut T, val, vp);
+	}
 
     pub fn get_absolute_offset<T>(rel_value : isize, mem : *mut T) -> *mut T
     {
@@ -304,7 +267,7 @@ pub mod injector
         p
     }
 
-    pub fn print_message(msg: &str) -> Result<i32, Error> {
+    pub fn print_message(msg: &str) -> Result<i32, std::io::Error> {
         use std::ffi::OsStr;
         use std::iter::once;
         use std::os::windows::ffi::OsStrExt;
@@ -314,7 +277,7 @@ pub mod injector
         let ret = unsafe {
             MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
         };
-        if ret == 0 { Err(Error::last_os_error()) }
+        if ret == 0 { Err(std::io::Error::last_os_error()) }
         else { Ok(ret) }
     }
 
